e3651a25f626b2b8a878b48ff96b3d27b65cdc37
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-02-16 11:27:00 -0800

    revise problem 9

--- exercise_9/exercise-9.sql
+++ exercise_9/exercise-9.sql
@@ -2,9 +2,23 @@
 -- LEFT JOIN FROM inventory to film and to rental
 -- use WHERE IS NULL
 
-SELECT title FROM film 
-RIGHT JOIN inventory
-ON film.film_id = inventory.film_id
-LEFT JOIN rental
-ON inventory.inventory_id = rental.inventory_id
-WHERE rental.rental_date IS NULL
+-- SELECT title FROM film 
+-- RIGHT JOIN inventory
+-- ON film.film_id = inventory.film_id
+-- LEFT JOIN rental
+-- ON inventory.inventory_id = rental.inventory_id
+-- WHERE rental.rental_date IS NULL
+
+with cte as (
+ select distinct
+  film_id,
+  rental_date
+  from inventory i
+  left join rental r on r.inventory_id = i.inventory_id
+ 
+)
+  select distinct 
+  	title 
+  from cte
+  join film f on f.film_id = cte.film_id
+  where cte.rental_date is null
