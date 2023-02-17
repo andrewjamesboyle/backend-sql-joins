57eb53cb4b137f570cadc015ab6d372e27d4756b
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-02-17 12:51:53 -0800

    solve all problems

--- exercise_15/exercise_15.sql
+++ exercise_15/exercise_15.sql
@@ -1,1 +1,5 @@
 -- the total run time of all inventory items if they were all played back to back
+
+SELECT SUM(length)
+FROM film f
+JOIN inventory i ON f.film_id = i.film_id
--- exercise_16/exercise_16.sql
+++ exercise_16/exercise_16.sql
@@ -1,3 +1,13 @@
 -- return the title, description, release year and list of actors returned as a JSON blob
 -- for the movie Airplane Sierra
     
+  SELECT title, description, release_year, 
+  COALESCE(JSON_AGG(to_jsonb(a.*))) as actors
+  FROM film f
+  LEFT JOIN film_actor fa
+  ON f.film_id = fa.film_id
+  LEFT JOIN actor a
+  ON fa.actor_id = a.actor_id
+  WHERE title = 'Airplane Sierra'
+  GROUP BY f.film_id
+
